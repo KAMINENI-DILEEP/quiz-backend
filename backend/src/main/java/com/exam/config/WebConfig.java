21ac82e242f57d3f8535ca7e48192a1f4d4afaee
KAMINENI-DILEEP <dileepkamineni@gmail.com>
2026-07-25 09:31:47 +0530

    Create WebConfig.java

---
+++ backend/src/main/java/com/exam/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.exam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                            "https://quizfrontend-kwmgimuwn-dileepkamineni-2530s-projects.vercel.app", 
+                            "http://localhost:3000", 
+                            "http://127.0.0.1:5500"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
