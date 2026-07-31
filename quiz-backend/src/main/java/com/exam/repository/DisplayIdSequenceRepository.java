4bc6dd981d7519b2e5c7569bee912f8983297a66
KAMINENI-DILEEP <dileepkamineni@gmail.com>
2026-08-01 00:00:30 +0530

    Delete quiz-backend directory

--- quiz-backend/src/main/java/com/exam/repository/DisplayIdSequenceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.exam.repository;
-
-import com.exam.model.DisplayIdSequence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisplayIdSequenceRepository extends JpaRepository<DisplayIdSequence, String> {}
--- quiz-backend/src/main/java/com/exam/repository/PortalSettingRepository.java
+++
@@ -1,4 +0,0 @@
-package com.exam.repository;
-import com.exam.model.PortalSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface PortalSettingRepository extends JpaRepository<PortalSetting, Long> {}
